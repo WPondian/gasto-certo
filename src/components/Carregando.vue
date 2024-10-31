10f930f48912b2962f2c9aca62ac851c8934f3ad
WPondian <willian.pondian@outlook.com>
2024-10-30 21:23:27 -0300

    'ajustando componente carregando'

--- src/components/Carregando.vue
+++ src/components/Carregando.vue
@@ -1,13 +1,19 @@
 <template>
     <div v-if="mostrar" data-dialog-backdrop="modal" data-dialog-backdrop-close="true"
         class="inset-0 z-[999] fixed grid h-screen w-screen place-items-center bg-black bg-opacity-60 backdrop-blur-sm transition-opacity">
-        <div class="w-1/3">
-            <span id="ProgressLabel" class="sr-only">Loading</span>
-
-            <span role="progressbar" aria-labelledby="ProgressLabel" aria-valuenow="75"
-                class="block rounded-full bg-gray-200">
-                <span class="block barra-de-progresso h-3 rounded-full" style="width: 100%"></span>
-            </span>
+        <div class="w-full">
+            <div id="box" class="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
+                <div class="box-circle">
+                    <svg>
+                        <circle cx="70" cy="70" r="70"></circle>
+                        <circle cx="70" cy="70" r="70"></circle>
+                    </svg>
+                </div>
+                <div class="carregando flex flex-col">
+                    <h2 class="font-semibold mt-5">Carregando...</h2>
+                    <img class="inline-block mt-3 coin" src="../assets/icon-carregando.png" alt="icone moeda rodando">
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -30,22 +36,72 @@ onUpdated(() => {
 </script>
 
 <style>
-.barra-de-progresso {
-    animation: progressAnimationStrike 5s infinite;
+#box {
+    background: rgba(0, 0, 0, 0.3);
+    border-radius: 20px;
+    padding: 15px;
 }
 
-@keyframes progressAnimationStrike {
-    from {
-        --tw-gradient-from: rgb(45 212 191) var(--tw-gradient-to-position);
-        --tw-gradient-to: #374151 var(--tw-gradient-from-position);
-        background-image: repeating-linear-gradient(50deg, var(--tw-gradient-from) 0, var(--tw-gradient-from) 20px, var(--tw-gradient-to) 20px, var(--tw-gradient-to) 40px);
+.box-circle svg {
+    width: 150px;
+    height: 150px;
+    position: relative;
+}
 
+circle {
+    width: 150px;
+    height: 150px;
+    fill: none;
+    stroke: black;
+    stroke-width: 10;
+    transform: translate(5px, 5px);
+    stroke-dasharray: 440;
+}
+
+circle:nth-child(1) {
+    stroke-dasharray: 440;
+    stroke-dashoffset: 0;
+    stroke: rgb(102, 102, 102);
+}
+
+circle:nth-child(2) {
+    stroke: rgb(45 212 191);
+    animation: rotacaoCarregando 5s forwards infinite;
+}
+
+.carregando {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+}
+
+.coin {
+    animation: rotacaoMoeda 2s linear infinite;
+}
+
+@keyframes rotacaoMoeda {
+    0% {
+        transform: rotateY(0deg);
     }
 
-    to {
-        --tw-gradient-from: #374151 var(--tw-gradient-from-position);
-        --tw-gradient-to: rgb(45 212 191) var(--tw-gradient-to-position);
-        background-image: repeating-linear-gradient(50deg, var(--tw-gradient-from) 0, var(--tw-gradient-from) 20px, var(--tw-gradient-to) 20px, var(--tw-gradient-to) 40px);
+    100% {
+        transform: rotateY(360deg);
+    }
+}
+
+@keyframes rotacaoCarregando {
+    0% {
+        stroke-dashoffset: 440;
+    }
+
+    100% {
+        stroke-dashoffset: 0;
     }
 }
 </style>
