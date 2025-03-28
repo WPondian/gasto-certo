6db4c2b064afe4f831a984d11475871714ca2174
WPondian <willian.pondian@outlook.com>
2025-03-28 18:01:07 -0300

    ajustando layout lista ganhos e adicionando imagens

--- src/components/ganho/ListaGanhos.vue
+++ src/components/ganho/ListaGanhos.vue
@@ -11,49 +11,49 @@
                 </div>
             </div>
             <div class="row flex items-center p-12 rounded-lg justify-center" style="background: #edf2f7;">
-                <section class="bg-white py-10 px-4 lg:px-16"> 
-                <div class="container mx-auto">
-                    <h1 class="text-2xl  text-center font-bold pb-12 sm:text-3xl uppercase">Atividades de ganho</h1>
-                    <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-28 lg:gap-y-16">
-                        <div class="relative group h-48 flex   flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-                            <a href="#" class="block">
-                                <div class="h-28">
-                                    <div
-                                        class="absolute -top-20 lg:top-[-10%] left-[5%] z-40  group-hover:top-[-40%] group-hover:opacity-[0.9]   duration-300 w-[90%] h-48 bg-blue-500 rounded-xl justify-items-center align-middle">
-                                        <img src="https://epicpadprinting.com/public/img/indus/Automotive.png"
-                                            class="w-36 h-36  mt-6 m-auto" alt="Automotive" title="Automotive" loading="lazy"
-                                            width="200" height="200">
+                <section class="bg-white py-10 px-4 lg:px-16 w-full"> 
+                    <div class="container mx-auto">
+                        <h1 class="text-2xl  text-center font-bold pb-12 sm:text-3xl uppercase">Atividades de ganho</h1>
+                        <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-28 lg:gap-y-16">
+                            <div class="relative group h-48 flex   flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                                <a href="#" class="block">
+                                    <div class="h-28">
+                                        <div
+                                            class="absolute -top-20 lg:top-[-10%] left-[5%] z-40  group-hover:top-[-40%] group-hover:opacity-[0.9]   duration-300 w-[90%] h-48 bg-blue-500 rounded-xl justify-items-center align-middle">
+                                            <img src="../../assets/ganho-trabalho.png"
+                                                class="w-36 h-36  mt-6 m-auto" alt="Automotive" title="Automotive" loading="lazy"
+                                                width="200" height="200">
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="p-6  z-10 w-full   ">
-                                    <p
-                                        class="mb-2 inline-block text-tg text-center w-full  text-xl  font-sans  font-semibold leading-snug tracking-normal   antialiased">
-                                        Emprego
-                                    </p>
-                                </div>
-                            </a>
-                        </div>
+                                    <div class="p-6 z-10 w-full">
+                                        <p
+                                            class="mb-2 inline-block text-tg text-center w-full text-xl font-sans font-semibold leading-snug tracking-normal   antialiased">
+                                            Emprego
+                                        </p>
+                                    </div>
+                                </a>
+                            </div>
 
-                        <div class="relative group h-48 flex   flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-                            <a href="#" class="block">
-                                <div class="h-28">
-                                    <div
-                                        class="absolute -top-20 lg:top-[-10%] left-[5%] z-40  group-hover:top-[-40%] group-hover:opacity-[0.9]   duration-300 w-[90%] h-48 bg-red-500 rounded-xl justify-items-center align-middle">
-                                        <img src="https://epicpadprinting.com/public/img/indus/Toys-and-Baby-Products.png"
-                                            class="w-36 h-36  mt-6 m-auto" alt="Toys and Baby Products"
-                                            title="Toys and Baby Products" loading="lazy" width="200" height="200">
+                            <div class="relative group h-48 flex flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                                <a href="#" class="block">
+                                    <div class="h-28">
+                                        <div
+                                            class="absolute -top-20 lg:top-[-10%] left-[5%] z-40 group-hover:top-[-40%] group-hover:opacity-[0.9]   duration-300 w-[90%] h-48 bg-red-500 rounded-xl justify-items-center align-middle">
+                                            <img src="../../assets/ganho-youtube.png"
+                                                class="w-36 h-36 mt-6 m-auto" alt="Toys and Baby Products"
+                                                title="Toys and Baby Products" loading="lazy" width="200" height="200">
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="p-6 z-10 w-full   ">
-                                    <p
-                                        class="mb-2 inline-block text-tg text-center w-full  text-xl  font-sans  font-semibold leading-snug tracking-normal   antialiased">
-                                        Youtube
-                                    </p>
-                                </div>
-                            </a>
+                                    <div class="p-6 z-10 w-full ">
+                                        <p
+                                            class="mb-2 inline-block text-tg text-center w-full text-xl font-sans font-semibold leading-snug tracking-normal   antialiased">
+                                            Youtube
+                                        </p>
+                                    </div>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
                 </section>
             </div>
         </div>
